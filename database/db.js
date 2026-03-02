652bfaba235bc45a7c07a6f8057e1c354792b1dd
Syedhassan <hassanshah1232611@gmail.com>
2026-03-02 22:55:25 +0500

    uri

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/Ekart`)
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log("MongoDB connect successfully");
     } catch (error) {
         console.log("MongoDB connection failed:", error);
